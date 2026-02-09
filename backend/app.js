404cae7e39898fb28a6ed062b7436cd80cb86959
blank_dignity <sakshyampaudel2@gmail.com>
2026-02-09 08:53:12 +0545

    Tinkering with database

--- backend/app.js
+++ backend/app.js
@@ -8,34 +8,19 @@ const connection = new Client({
   host: "localhost",
   user: "postgres",
   password: "sakshyam",
-  database: "mydb",
   port: 5432,
+  database: "mydb",
 });
 
-connection.connect().then(() => {
+connection.connect(undefined).then(() => {
   console.log("Connected to the database.");
 })
 
-const { id, name } = { id: 11, name: "Sam" };
-
-const insert_query = "INSERT INTO users (id, name) VALUES ($1, $2)";
-connection.query(insert_query, [id, name], (err, result) => {
-  if (err)
-    console.log(err);
-  else {
-    console.log(result);
-    console.log("Sent");
-  }
-
+app.get('/', async (req, res) => {
+  // const result = await connection.query("SELECT * FROM users;");
+  // console.log(result.rows[0]);
+  res.send('Test');
 });
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-// app.use(express.static('public'));
-// app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
