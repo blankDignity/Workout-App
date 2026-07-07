4f27b4bf2e83bf358451d7bd2208f1e422e790ac
blank_dignity <sakshyampaudel2@gmail.com>
2026-07-07 19:38:18 +0545

    resolved svg not rendering

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,13 +1,20 @@
 import {NavLink} from "react-router-dom"
 import type {SidebarProps} from "../types/sidebar.ts";
-
+import dumbbell from "../assets/dumbbell.svg"
+import home from "../assets/home.svg"
+import workoutPlan from "../assets/workoutPlan.svg"
+import addWorkout from "../assets/addWorkout.svg"
+import about from "../assets/about.svg"
+import user from "../assets/usr.png"
+import progress from "../assets/progress.svg"
+import settings from "../assets/settings.svg"
 
 const navItems: SidebarProps[] = [
-  {id: "home", to: "/", label: "Home", icon: "home.svg"},
-  {id: "workout", to: "/workout", label: "Workout", icon: "dumbbell.svg"},
-  {id: "workoutPlans", to: "/workoutPlans", label: "Exercise Plans", icon: "workoutPlan.svg"},
-  {id: "addWorkout", to: "/addWorkout", label: "Add Workout", icon: "addWorkout.svg"},
-  {id: "about", to: "/about", label: "About", icon: "about.svg"},
+  {id: "home", to: "/", label: "Home", icon: home},
+  {id: "workout", to: "/workout", label: "Workout", icon: dumbbell},
+  {id: "workoutPlans", to: "/workoutPlans", label: "Exercise Plans", icon: workoutPlan},
+  {id: "addWorkout", to: "/addWorkout", label: "Add Workout", icon: addWorkout},
+  {id: "about", to: "/about", label: "About", icon: about},
 ]
 
 function Sidebar() {
@@ -17,9 +24,8 @@ function Sidebar() {
 
           <div className="grid grid-cols-[auto_1fr] gap-3 items-center">
             <div className="w-10 h-10 bg-red-400 rounded-2xl p-2 flex items-center justify-center">
-              <img src="../../src/assets/dumbbell.svg" alt="logo" className="w-full h-full"/>
+              <img src={dumbbell} alt={"dumbbell"}/>
             </div>
-
             <div className="flex flex-col leading-tight">
               <span className="text-2xl font-bold text-white">FITFORGE</span>
               <span className="text-[#8b8b95] font-light text-sm">Train Harder</span>
@@ -30,7 +36,7 @@ function Sidebar() {
             {navItems.map((item) => (<NavLink id={item.id} to={item.to}
                                               className={({isActive}) => `flex items-center gap-3 [#ff4757] rounded-xl hover:text-white transition
               ${isActive ? "bg-[#ff4757] text-white font-semibold" : "bg-transparent hover:bg-zinc-800"}`}>
-              <img src={`../../src/assets/${item.icon}`} alt={item.id} className={"w-5 ml-3"}/>
+              <img src={item.icon} alt={item.id} className={"w-5 ml-3"}/>
               {item.label}
             </NavLink>))}
           </div>
@@ -39,19 +45,19 @@ function Sidebar() {
               className="border-y border-zinc-800 pt-4 flex flex-col gap-5 *:py-2 text-zinc-400">
             <div
                 className={"flex items-center gap-3 hover:bg-[#ff4757] rounded-xl transition hover:text-white cursor-pointer"}>
-              <img src={"../../src/assets/progress.svg"} alt={"progress"} className={"w-5 ml-3"}/>
+              <img src={progress} alt={"progress"} className={"w-5 ml-3"}/>
               <div>Progress</div>
             </div>
             <div
                 className={"flex items-center gap-3 hover:bg-[#ff4757] rounded-xl transition hover:text-white cursor-pointer mb-3"}>
-              <img src={"../../src/assets/settings.svg"} alt={"setting"} className={"w-5 ml-3"}/>
+              <img src={settings} alt={"setting"} className={"w-5 ml-3"}/>
               <div>Settings</div>
             </div>
           </div>
 
           <div className="grid grid-cols-[auto_1fr]  gap-3 items-center">
             <div className="w-10 h-10 bg-red-300 rounded-2xl p-2 flex items-center justify-center">
-              <img src="../../src/assets/usr.png" alt="logo"
+              <img src={user} alt="logo"
                    className="rounded-full object-cover aspect-square"/>
             </div>
 
